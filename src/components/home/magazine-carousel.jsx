dee5dff21bfc29c93a2aa936a12bc41702b693ad
bbleak171 <jpang.monthicha@gmail.com>
2025-12-24 20:47:40 +0700

    magazine update hight

--- src/components/home/magazine-carousel.jsx
+++ src/components/home/magazine-carousel.jsx
@@ -46,7 +46,7 @@ export default function MagazineCarousel({ data }) {
                       <img
                         src={item.image}
                         alt={item.title}
-                        className="h-[390px] object-cover"
+                        className="h-[360px] object-cover"
                       />
                     </figure>
 
