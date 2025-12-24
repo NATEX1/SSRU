6a6df2660207f775647c805779fe398ccf2e3618
bbleak171 <jpang.monthicha@gmail.com>
2025-12-24 20:37:18 +0700

    update h-card

--- src/components/home/magazine-carousel.jsx
+++ src/components/home/magazine-carousel.jsx
@@ -46,7 +46,7 @@ export default function MagazineCarousel({ data }) {
                       <img
                         src={item.image}
                         alt={item.title}
-                        className="h-[300px] object-cover"
+                        className="h-[390px] object-cover"
                       />
                     </figure>
 
