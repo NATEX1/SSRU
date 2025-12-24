0168ca95626b81b427fbfe92c1bb33cbdd58ea84
bbleak171 <jpang.monthicha@gmail.com>
2025-12-24 22:24:24 +0700

    change link slide

--- src/components/article-slider.jsx
+++ src/components/article-slider.jsx
@@ -139,7 +139,7 @@ export default function ArticleSlider() {
         <div className="grid md:grid-cols-2">
           <div className="relative aspect-video bg-gray-100 overflow-hidden rounded-lg">
             <a
-              href={`/${articles.slug}`}
+              href={`/${current.slug}`}
               className="col-span-2 block"
             >
               <AnimatePresence custom={direction} mode="popLayout" initial={false}>
@@ -173,7 +173,7 @@ export default function ArticleSlider() {
                 </p>
 
                 <a
-                  href={`/${articles.slug}`}
+                  href={`/${current.slug}`}
                   className="col-span-2 block"
                 >
                   <h2 className="text-2xl font-bold text-gray-900 mb-4 leading-tight">
