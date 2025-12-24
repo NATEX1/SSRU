5e1f9d40468dbda8da10a855a838d165bec858e0
bbleak171 <jpang.monthicha@gmail.com>
2025-12-24 20:06:01 +0700

    update magazine-carousel.jsx

--- src/components/home/magazine-carousel.jsx
+++ src/components/home/magazine-carousel.jsx
@@ -46,7 +46,7 @@ export default function MagazineCarousel({ data }) {
                       <img
                         src={item.image}
                         alt={item.title}
-                        className="h-[400px] object-cover"
+                        className="h-[306px] object-cover"
                       />
                     </figure>
 
