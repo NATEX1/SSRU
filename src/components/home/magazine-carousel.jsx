610c5c87aa6b132e456fc4db3ae1f4070bb41c2a
bbleak171 <jpang.monthicha@gmail.com>
2025-12-24 20:16:30 +0700

    update magazine card

--- src/components/home/magazine-carousel.jsx
+++ src/components/home/magazine-carousel.jsx
@@ -46,7 +46,7 @@ export default function MagazineCarousel({ data }) {
                       <img
                         src={item.image}
                         alt={item.title}
-                        className="h-[320px] object-cover"
+                        className="h-[360px] object-cover"
                       />
                     </figure>
 
