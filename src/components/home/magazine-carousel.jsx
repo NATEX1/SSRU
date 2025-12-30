f7cc753de5c9d87291a6feb22fbe754cf6b09102
bbleak171 <jpang.monthicha@gmail.com>
2025-12-31 00:20:05 +0700

    test

--- src/components/home/magazine-carousel.jsx
+++ src/components/home/magazine-carousel.jsx
@@ -28,9 +28,8 @@ export default function MagazineCarousel({ data }) {
         <div className="w-full overflow-hidden">
           <Swiper
             modules={[Grid]}
-            // slidesPerView={2}
-            spaceBetween={16}
-            slidesPerView={1}        // ✅ มือถือ = 1 อันต่อแถว
+            spaceBetween={30}
+            slidesPerView={1.15}       
             breakpoints={{
               640: { slidesPerView: 2 },
               1024: { slidesPerView: 3 },
